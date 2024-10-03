3a5526147f202e25214f9e595aaaa6b5e1a96450
Joey Herring <joey.herring@shopify.com>
2024-10-03 14:58:07 +1300

    Adding welcome page

--- application/app/root.tsx
+++ application/app/root.tsx
@@ -8,6 +8,7 @@ import {
 import type { LinksFunction } from "@remix-run/node";
 
 import "./tailwind.css";
+import "./styles/custom.css";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -18,7 +19,15 @@ export const links: LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://cdn.shopify.com/static/fonts/ShopifySans/v1.0/ShopifySans-Regular.woff2",
+    type: "font/woff2",
+    crossOrigin: "anonymous",
+  },
+  {
+    rel: "stylesheet",
+    href: "https://cdn.shopify.com/static/fonts/ShopifySans/v1.0/ShopifySans-Bold.woff2",
+    type: "font/woff2",
+    crossOrigin: "anonymous",
   },
 ];
 
